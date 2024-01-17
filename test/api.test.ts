ada8bd46477446395cb00ab4228d60aeb2cfbccc
Yusuf Azam <yusufazam95@hotmail.co.uk>
2024-01-16 19:44:12 -0600

    init cdk project

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as Infra from '../lib/infra-stack';
+// import * as Api from '../lib/api-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/infra-stack.ts
+// example resource in lib/api-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new Infra.InfraStack(app, 'MyTestStack');
+//   const stack = new Api.ApiStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
